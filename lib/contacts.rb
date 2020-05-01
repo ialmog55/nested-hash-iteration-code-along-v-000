3870765026b4c79862968c6cb7fbfadd3753fa4b
idith almog <almog.idith@gmail.com>
2020-05-01 21:14:01 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,7 +13,23 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
+  contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
 def remove_strawberry(contacts)
-
+  contacts.each do |person,data|
+    if person ==   "Freddy Mercury"
+      data[:favorite_ice_cream_flavors].shift("strawberry")
+    end
+  end
+contacts
 end
