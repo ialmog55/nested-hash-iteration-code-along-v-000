bd5443d528d2dc4d63d8f79cb8696b147bd7e31f
idith almog <almog.idith@gmail.com>
2020-05-02 01:25:52 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -28,6 +28,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person,data|
     if person ==   "Freddy Mercury"
+      binding.pry
       data.each do |attribute,value|
         if attribute == "favorite_ice_cream_flavors:"
           value.shift
